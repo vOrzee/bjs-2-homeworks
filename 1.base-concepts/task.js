a3e61b6fbda0942b028a6cdcd86eb9c3229fbc9b
Roman Leshin <vorzee@gmail.com>
2025-02-09 14:09:24 +0500

    Update task.js
    Домашнее задание к лекции 1 «Основные понятия»

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else if (d > 0) {
+    let sqrt = Math.sqrt(d);
+    arr[0] = (-b + sqrt) / (2 * a);
+    arr[1] = (-b - sqrt) / (2 * a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthKoef = percent / 100 / 12;
+  const bodyAmounth = amount - contribution;
+  const monthPayment = bodyAmounth * (monthKoef + (monthKoef / (((1 + monthKoef) ** countMonths) - 1)));
+  return Number((monthPayment * countMonths).toFixed(2));
 }

3c4001f92b3bf4efa478cdf71b624297a6352024
Roman Leshin <vorzee@gmail.com>
2025-02-10 16:22:19 +0500

    HW5
    Домашнее задание к лекции 4 «Объекты»

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if("marks" in this && Array.isArray(this.marks)) {
+  	this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if("marks" in this && Array.isArray(this.marks) && this.marks.length !== 0) {
+  	return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
+  } else {
+  	return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
 }

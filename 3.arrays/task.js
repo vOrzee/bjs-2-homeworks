f6f3e328e9ce622b8066be580deaf709730be805
Roman Leshin <vorzee@gmail.com>
2025-02-10 15:55:53 +0500

    HW4
    Домашнее задание к лекции 3 «Массивы»

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, index) => arr2[index] === element);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter(user => user.gender === gender)
+    .reduce((acc, user, index, array) => 
+      index === array.length - 1 ? ((acc + user.age) / array.length) : (acc + user.age)
+    , 0);
 }

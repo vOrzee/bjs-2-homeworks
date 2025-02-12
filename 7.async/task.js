69c34324ce7d98244fb96b9e8c7f1b0c9ca45dea
Roman Leshin <vorzee@gmail.com>
2025-02-12 13:57:51 +0500

    HW8
    Fix

--- 7.async/task.js
+++ 7.async/task.js
@@ -23,10 +23,10 @@ class AlarmClock {
 	}
 
 	getCurrentFormattedTime() {
-	    const now = new Date();
-	    const hours = String(now.getHours()).padStart(2, '0');
-	    const minutes = String(now.getMinutes()).padStart(2, '0');
-	    return `${hours}:${minutes}`;
+		const now = new Date();
+		const hours = String(now.getHours()).padStart(2, '0');
+		const minutes = String(now.getMinutes()).padStart(2, '0');
+		return `${hours}:${minutes}`;
 	}
 
 	start() {

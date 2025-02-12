c3ad7d6fcb69f0768b6b68f9de45cef8b41391b9
Roman Leshin <vorzee@gmail.com>
2025-02-12 11:44:04 +0500

    HW7
    Домашнее задание к лекции 6 «Обработка исключений и замыкания»

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+    const result = Number.parseFloat(value);
+    
+    if (Number.isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    
+    return result;
+}
+
+function validateCount(value) {
+    try {
+    	return parseCount(value);	
+    } catch (error) {
+    	return error;
+    }
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (c > a + b || a > b + c || b > a + c) {
+			throw new Error("Треугольник с такими сторонами не существует")
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = (this.a + this.b + this.c) / 2
+		return Number(((p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+    try {
+    	return new Triangle(a, b, c);	
+    } catch (error) {
+    	return {
+    		get perimeter() { return "Ошибка! Треугольник не существует"; },
+    		get area() { return "Ошибка! Треугольник не существует"; }
+    	};
+    }
+}

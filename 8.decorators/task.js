187067fa70401f2fdee57e3b7e6904f13232fd32
Roman Leshin <vorzee@gmail.com>
2025-02-12 17:34:54 +0500

    HW9
    Домашнее задание к лекции 8 «Декораторы»

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = []
+  return function(...args) {
+  	const hash = md5([...args]);
+    const cachedElement = cache.find(it => it.hash === hash);
+    if (cachedElement) {
+      return `Из кеша: ${cachedElement.value}`;
+    }
+  	const value = func(...args);
+  	
+  	if (cache.length === 5) {
+  		cache.shift();
+  	}
+
+  	cache.push({hash, value});
+  	return `Вычисляем: ${value}`;
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	let isFirstCall = true;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+
+		if (isFirstCall) {
+			func(...args);
+			wrapper.count++;
+			isFirstCall = false;
+			return;
+		}
+
+		clearTimeout(timeoutId);
+
+		timeoutId = setTimeout(() => {
+			func(...args);
+			wrapper.count++;
+		}, delay);
+	}
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	return wrapper;
 }
+
